444be1911fe967cd65932ac32a3c18e057319433
Kirk Owst <kirkowst@gmail.com>
2023-09-10 13:16:48 +0100

    Mostly finished code

--- motor_code.py
+++ motor_code.py
@@ -32,13 +32,13 @@ def on_release(key):
 #listener.start()
 
 def main():
-    folder = user_settings()
+    settings = user_settings()
     Motor1 = DRV8825(step_angle=1.8 ,dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
     Motor2 = DRV8825(step_angle=1.8 ,dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
     # Motor2 = DRV8825(step_angle=1.8 ,dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
     try:
-        user_interface(film_motor=Motor1, takeup_motor=Motor2, image_folder=folder)
+        user_interface(film_motor=Motor1, takeup_motor=Motor2, settings=settings)
     except Exception as e:
         GPIO.cleanup()
         print("\nMotor stop")
@@ -64,17 +64,29 @@ def user_settings(settings_file='./settings.json'):
             settings = json.load(file)
     
     settings['subfolder'] = input("(Default: \"Movie1\")") or "Movie1"
-    movie_folder = f"{settings['folder']}/{settings['subfolder']}"
-    if not os.path.isdir(movie_folder):
-        os.makedirs(movie_folder)
-
-    return movie_folder
+    settings['movie_folder'] = f"{settings['folder']}/{settings['subfolder']}"
+    settings['film_type'] = input("Super 8? [y/n] (Default: n)").lower() or 'n'
+    settings['film_length'] = input("What is your reel length? (Default: 50)") or 50
 
 
+    if os.path.isdir(settings['movie_folder']):
+        dir = os.listdir(settings['movie_folder'])
+        nums = sorted([ int(num.split('.')[0]) for num in dir]) #split the names and sort numbers
+        settings['latest_pic'] = nums[-1]
+        
+        # final_output = [ str(i)+".png" for i in nums] #append file extension and create another list.
+        # print(final_output)
+    else:
+        os.makedirs(settings['movie_folder'])
 
-def user_interface(film_motor, takeup_motor, image_folder='./'):
+
+    return settings
+
+
+
+def user_interface(film_motor, takeup_motor, settings):
     # Setup with camera controls
-    film_motor.SetMicroStep('softward','1/4step')
+    film_motor.SetMicroStep('softward','fullstep')
     takeup_motor.SetMicroStep('softward', 'fullstep')
     
     # film_motor.TurnStep(Dir='forward', degrees=360)
@@ -85,6 +97,10 @@ def user_interface(film_motor, takeup_motor, image_folder='./'):
     camera.resolution = (4056, 3040)
     camera.start_preview()
     index = 0
+
+    if 'latest_pic' in settings:
+        # print(settings['latest_pic'])
+        index = settings['latest_pic']
 
     with keyboard.Events() as events:
         for event in events:
@@ -109,7 +125,7 @@ def user_interface(film_motor, takeup_motor, image_folder='./'):
                     dir = 'backward'
                     if mode == 0:
                         film_motor.LoopSetup(dir)
-                        film_motor.TurnFrames(dir, 1)
+                        film_motor.TurnFrames(dir, 5)
                     elif mode == 2:
                         film_motor.TurnStep(dir, 1)
                         # move_motor_contiuously(film_motor, dir)  
@@ -117,9 +133,9 @@ def user_interface(film_motor, takeup_motor, image_folder='./'):
                 if event.key == keyboard.KeyCode().from_char('c'):
                     if mode == 2:
                         # Camera warm-up time
-                        image = f'{image_folder}/{index}.png'
+                        settings['image'] = f'{settings["movie_folder"]}/{index}.png'
 
-                        take_picture(film_motor, takeup_motor, camera, image)
+                        take_picture(film_motor, takeup_motor, camera, settings)
                         index += 1
 
                         # Take a picture then next frame
@@ -135,13 +151,33 @@ def user_interface(film_motor, takeup_motor, image_folder='./'):
                         
                 print('Receivedevent {}'.format(event))
     
+    if settings['film_type'] == 'n':
+        frames = settings['film_length'] * 80
+    elif settings['film_type'] == 'y': 
+        frames = settings['film_length'] * 72
+    else:
+        print('Wrong film type')
+        Exception()
+
     try:
         while True:
-            image = f'{image_folder}/{index}.png'
-            take_picture(film_motor, takeup_motor, camera, image)
+            if frames == index + 6:
+                break
+            settings['image'] = f'{settings["movie_folder"]}/{index}.png'
+            take_picture(film_motor, takeup_motor, camera, settings)
             index += 1
-    except KeyboardInterrupt:
+
         print(f"Reverse Film by {index} frames")
+        for i in range(index):
+            takeup_motor.TurnStep('backward', 6)
+            takeup_motor.Stop() 
+
+            film_motor.TurnFrames('backward', 1)
+            film_motor.Stop()
+    except KeyboardInterrupt:
+        film_motor.Stop()
+        takeup_motor.Stop() 
+                
 
 
 
@@ -151,19 +187,21 @@ def move_motor_contiuously(motor, dir='forward'):
     time.sleep(motor.delay)
     motor.digital_write(motor.step_pin, False)
 
-def take_picture(film_motor, takeup_motor, camera, image_path):
-    print(image_path)
-    # takeup_motor.SetMicroStep('softward', 'fullstep')
-    # takeup_motor.delay = .005 * 3
-    # takeup_motor.digital_write(takeup_motor.dir_pin, 1)
-    
-    time.sleep(2)
-    # camera.capture(image_path)
-    print("Moving frames!") 
-    # film_motor.TurnFrames('forward', 1)
-    # film_motor.Stop() 
+def take_picture(film_motor, takeup_motor, camera, settings):
+    print(settings['image'])
 
-    takeup_motor.TurnStep('backward', 5)
+    dir = 'forward'
+    camera.exposure_mode = 'off'
+    camera.awb_mode = 'auto'
+    camera.ISO = 100
+    time.sleep(2)
+    camera.capture(settings['image'])
+    print("Moving frames!") 
+    film_motor.delay = 1/800
+    film_motor.TurnFrames(dir, 1)
+    film_motor.Stop() 
+
+    takeup_motor.TurnStep(dir, 3)
     takeup_motor.Stop() 
                 
 def setup_motors():
